08f12b46ac31f765be9a826c2aed7862f5921525
Mateusz <cebula.mat@gmail.com>
2018-03-14 23:38:16 +0100

    initial commit

---
+++ GeoLib.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GeoLib.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GeoLib.Core")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2f958cae-faf9-418e-8943-e3fe2b15f915")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GeoLib.Data/RepositoryInterfaces/IZipCodeRepository.cs
@@ -0,0 +1,17 @@
+﻿using GeoLib.Core;
+using GeoLib.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLib.Data.RepositoryInterfaces
+{
+    public interface IZipCodeRepository : IDataRepository
+    {
+        ZipCode GetByZip(string zip);
+        IEnumerable<ZipCode> GetByState(string state);
+        IEnumerable<ZipCode> GetZipsForRange(ZipCode zip, int range);
+    }
+}
