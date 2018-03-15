f3a7e467c45424ab965a891388dca9d21291fc4a
Mateusz <cebula.mat@gmail.com>
2018-03-15 09:33:18 +0100

    add Contracts, Service, Host and Client

---
+++ GeoLib.ConsoleHost/Program.cs
@@ -0,0 +1,36 @@
+﻿using GeoLib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLib.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
+
+            ServiceMetadataBehavior metadataBehavior = hostGeoManager.Description.Behaviors.Find<ServiceMetadataBehavior>();
+            if (metadataBehavior == null)
+            {
+                metadataBehavior = new ServiceMetadataBehavior();
+                metadataBehavior.HttpGetEnabled = true;
+                hostGeoManager.Description.Behaviors.Add(metadataBehavior);
+            }
+
+            hostGeoManager.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "MEX");
+
+            hostGeoManager.Open();
+
+            Console.WriteLine("Services started. Press [Enter] to exit.");
+            Console.ReadLine();
+
+            hostGeoManager.Close();
+        }
+    }
+}
---
+++ GeoLib.Data/RepositoryInterfaces/IStateRepository.cs
@@ -0,0 +1,16 @@
+﻿using GeoLib.Core;
+using GeoLib.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLib.Data.RepositoryInterfaces
+{
+    public interface IStateRepository : IDataRepository<State>
+    {
+        State Get(string abbrev);
+        IEnumerable<State> Get(bool primaryOnly);
+    }
+}
